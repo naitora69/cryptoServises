4f12ae08af886cc67089c52ec4de3b1df0f7d58b
danila <danila.kuryakin@gmail.com>
2026-01-04 13:37:35 +0300

    add migrations

--- governance-indexer/go.mod
+++ governance-indexer/go.mod
@@ -3,6 +3,7 @@ module governance-indexer
 go 1.25
 
 require (
+	github.com/golang-migrate/migrate/v4 v4.19.1
 	github.com/lib/pq v1.10.9
 	gopkg.in/yaml.v2 v2.4.0
 )
--- governance-indexer/internal/repository/postgres.go
+++ governance-indexer/internal/repository/postgres.go
@@ -2,6 +2,13 @@ package repository
 
 import (
 	"database/sql"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 type PostgresConfig struct {
@@ -11,6 +18,30 @@ type PostgresConfig struct {
 	Port     string
 	Name     string
 	SSLMode  string
+}
+
+func RunMigrations(dsn string) error {
+
+	_, filename, _, _ := runtime.Caller(0)
+
+	// internal/repository → в корень → migrations
+	path := filepath.Join(filepath.Dir(filename), "../../migrations")
+
+	// делаем URL-совместимый путь
+	path = strings.ReplaceAll(path, "\\", "/")
+
+	m, err := migrate.New(
+		"file://"+path,
+		dsn,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+	return nil
 }
 
 // PostgresDSN (Data Source Name) формирует строку подключения PostgreSQL
@@ -23,6 +54,9 @@ func PostgresDSN(config PostgresConfig) string {
 
 func NewPostgresDB(config PostgresConfig) (*sql.DB, error) {
 	dsn := PostgresDSN(config)
+	if err := RunMigrations(dsn); err != nil {
+		return nil, err
+	}
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
