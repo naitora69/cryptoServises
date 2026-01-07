a4ea9f6ae751a6337b57be58a734d6a532931ed2
naitora69 <aominedaikiii192@gmail.com>
2026-01-08 00:12:46 +0300

    add_live_token_analyze

---
+++ scam-analyzator/go.mod
@@ -0,0 +1,5 @@
+module scam-analyzator-service
+
+go 1.24.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ scam-analyzator/internal/scamanalyzator/age-analyze.go
@@ -0,0 +1,31 @@
+package scamanalyzator
+
+import (
+	"time"
+)
+
+type AnalysisResult struct {
+	IsNewToken bool      `json:"is_new_token"` // вердикт
+	AgeHours   float64   `json:"age_hours"`    // возраст в часах для наглядности
+	CreatedAt  time.Time `json:"created_at"`   // дата создания в читаемом виде
+}
+
+func AnalyzeAge(timestamp int64) AnalysisResult {
+	// конвертируем Unix (секунды) в объект времени Go
+	createdTime := time.Unix(timestamp, 0)
+
+	// считаем разницу между сейчас и временем создания
+	duration := time.Since(createdTime)
+
+	// переводим разницу в часы
+	hours := duration.Hours()
+
+	// наш критерий скама - токену меньше 24 часов
+	isScam := hours < 24
+
+	return AnalysisResult{
+		IsNewToken: isScam,
+		AgeHours:   hours,
+		CreatedAt:  createdTime,
+	}
+}
